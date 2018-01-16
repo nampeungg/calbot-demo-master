e87156aca217756d9c9cc91157cc85895cec7927
nampeung <nampeungg@gmail.com>
2018-01-16 22:39:59 +0700

    2
    2

--- app.js
+++ app.js
@@ -104,14 +104,14 @@ bot.dialog('/', [
     function (session) {
         // Send a greeting and show help.
         var card = new builder.HeroCard(session)
-            .title("Microsoft Bot Framework")
-            .text("Your bots - wherever your users are talking.")
+            .title("NP Shop")
+            .text("All the clothes you need")
             .images([
-                 builder.CardImage.create(session, "http://docs.botframework.com/images/demo_bot_image.png")
+                 builder.CardImage.create(session, "https://image.ibb.co/kL4Ce6/online_store.png")
             ]);
         var msg = new builder.Message(session).attachments([card]);
         session.send(msg);
-        session.send("Hi... I'm the Microsoft Bot Framework demo bot for Facebook. I can show you everything you can use our Bot Builder SDK to do on Facebook.");
+        session.send("Hi... Welcome to NP Shop.");
 
         session.beginDialog('/help');
     },
@@ -127,7 +127,7 @@ bot.dialog('/', [
 
 bot.dialog('/menu', [
     function (session) {
-        builder.Prompts.choice(session, "What demo would you like to run?", "prompts|picture|cards|list|carousel|receipt|actions|(quit)");
+        builder.Prompts.choice(session, "What do you want to ask?", "Detail of Products|Recommendation|FAQ|(quit)");
     },
     function (session, results) {
         if (results.response && results.response.entity != '(quit)') {
@@ -150,7 +150,55 @@ bot.dialog('/help', [
     }
 ]);
 
-bot.dialog('/prompts', [
+bot.dialog('/Detail of Products', [
+    function (session) {
+        session.send("What do you want to know about our products? Please tell me.")
+        builder.Prompts.choice(session, "Prompts.choice()\n\nPlease Choose: ", "Price|Stock|Size|Color|none");
+    },
+    function (session, results) {
+        session.send("You want to know about the "+results.response.entity+"...")
+        if (results.response.entity == "Price") {
+            session.beginDialog('askPrice');
+        }
+    }
+]);
+
+bot.dialog('askPrice', [
+    function (session) {
+        builder.Prompts.text(session,"Enter Product Name: ");
+    },
+    function (session,results){
+        session.send(results.response);
+        executeStatement(session,"SELECT * FROM Products WHERE Name = 'Oxford Shirt'");
+    }
+]);
+
+
+function executeStatement(session,sql_query) {  
+    request = new Request(sql_query, function(err) {  
+    if (err) {  
+        session.send(err);}  
+    });  
+    var result = "";  
+    request.on('row', function(columns) {  
+        columns.forEach(function(column) {  
+          if (column.value === null) {  
+            session.send('NULL');  
+          } else {  
+            result+= column.value + " ";  
+          }  
+        });  
+        session.send(result);  
+        result ="";  
+    });  
+
+    request.on('doneInProc', function(rowCount, more) {  
+    session.send(rowCount + ' rows returned');  
+    });  
+    connection.execSql(request);  
+}  
+
+/*bot.dialog('/prompts', [
     function (session) {
         session.send("Our Bot Builder SDK has a rich set of built-in prompts that simplify asking the user a series of questions. This demo will walk you through using each prompt. Just follow the prompts and you can quit at any time by saying 'cancel'.");
         builder.Prompts.text(session, "Prompts.text()\n\nEnter some text and I'll say it back.");
