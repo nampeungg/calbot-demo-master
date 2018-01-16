66c8f9e08c530c5d7727b8d7092475d04476a309
nampeung <nampeungg@gmail.com>
2018-01-16 22:44:46 +0700

    4
    4

--- app.js
+++ app.js
@@ -1,45 +1,3 @@
-/*-----------------------------------------------------------------------------
-This Bot uses the Bot Connector Service but is designed to showcase whats 
-possible on Facebook using the framework. The demo shows how to create a looping 
-menu how send things like Pictures, Bubbles, Receipts, and use Carousels. It also
-shows all of the prompts supported by Bot Builder and how to receive uploaded
-photos, videos, and location.
-
-# RUN THE BOT:
-
-    You can run the bot locally using the Bot Framework Emulator but for the best
-    experience you should register a new bot on Facebook and bind it to the demo 
-    bot. You can run the bot locally using ngrok found at https://ngrok.com/.
-
-    * Install and run ngrok in a console window using "ngrok http 3978".
-    * Create a bot on https://dev.botframework.com and follow the steps to setup
-      a Facebook channel. The Facebook channel config page will walk you through 
-      creating a Facebook page & app for your bot.
-    * For the endpoint you setup on dev.botframework.com, copy the https link 
-      ngrok setup and set "<ngrok link>/api/messages" as your bots endpoint.
-    * Next you need to configure your bots MICROSOFT_APP_ID, and
-      MICROSOFT_APP_PASSWORD environment variables. If you're running VSCode you 
-      can add these variables to your the bots launch.json file. If you're not 
-      using VSCode you'll need to setup these variables in a console window.
-      - MICROSOFT_APP_ID: This is the App ID assigned when you created your bot.
-      - MICROSOFT_APP_PASSWORD: This was also assigned when you created your bot.
-    * Install the bots persistent menus following the instructions outlined in the
-      section below.
-    * To run the bot you can launch it from VSCode or run "node app.js" from a 
-      console window. 
-
-# INSTALL PERSISTENT MENUS
-
-    Facebook supports persistent menus which Bot Builder lets you bind to global 
-    actions. These menus must be installed using the page access token assigned 
-    when you setup your bot. You can easily install the menus included with the 
-    example by running the cURL command below:
-
-        curl -X POST -H "Content-Type: application/json" -d @persistent-menu.json 
-        "https://graph.facebook.com/v2.6/me/thread_settings?access_token=PAGE_ACCESS_TOKEN"
-    
------------------------------------------------------------------------------*/
-
 var restify = require('restify');
 var builder = require('botbuilder');
 
@@ -66,6 +24,8 @@ var config = {
 var connection = new Connection(config);  
 connection.on('connect', function(err) {  
 });
+var Request = require('tedious').Request;  
+var TYPES = require('tedious').TYPES;  
 
 // Bot Storage: Here we register the state storage for your bot. 
 // Default store: volatile in-memory store - Only for prototyping!
@@ -94,11 +54,6 @@ bot.set(builder.Middleware.dialogVersion({ version: 1.0, resetCommand: /^reset/i
 
 bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^goodbye/i });
 bot.beginDialogAction('help', '/help', { matches: /^help/i });
-
-
-//=========================================================
-// Bots Dialogs
-//=========================================================
 
 bot.dialog('/', [
     function (session) {
